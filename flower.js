f9479807f7bf1ac0c02982bc82263d29d7baad04
JoelP-11 <Jp421513@gmail.com>
2024-08-15 22:03:13 -0700

    finished

--- flower.js
+++ flower.js
@@ -1,6 +1,11 @@
-onload = () => {
+window.onload = () => {
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
     }, 1000);
+
+    document.getElementById('start-button').addEventListener('click', function() {
+      document.getElementById('initial-screen').style.display = 'none';
+      document.getElementById('flower-screen').style.display = 'block';
+    });
   };
